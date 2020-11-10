1444d613399fc244b837d80740d007d275d18a13
Jabberski <chalasiewicz97@gmail.com>
2020-11-10 16:54:58 +0100

    AdminDao Querys hotfix

--- src/main/java/pl/coderslab/dao/AdminDao.java
+++ src/main/java/pl/coderslab/dao/AdminDao.java
@@ -14,12 +14,12 @@ import java.util.List;
 
 
 public class AdminDao  {
-    private static final String createAdminQuery = "INSERT INTO admins(first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
-    private static final String deleteAdminQuery = "DELETE FROM admins WHERE id = ?";
-    private static final String findAllAdminsQuery = "SELECT * FROM admins";
-    private static final String updateAdminQuery = "UPDATE admins SET first_name=?, last_name=?, email=?, password = ? WHERE id=?";
-    private static final String changeSuperadminQuery = "UPDATE admins SET superadmin=? WHERE id=?";
-    private static final String findAdminQuery = "SELECT * FROM admins WHERE id=?";
+    private static final String CREATE_ADMIN_QUERY = "INSERT INTO admins(first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
+    private static final String DELETE_ADMIN_QUERY = "DELETE FROM admins WHERE id = ?";
+    private static final String FIND_ALL_ADMIN_QUERY = "SELECT * FROM admins";
+    private static final String UPDATE_ADMIN_QUERY= "UPDATE admins SET first_name=?, last_name=?, email=?, password = ? WHERE id=?";
+    private static final String CHANGE_SUPER_ADMIN_QUERY = "UPDATE admins SET superadmin=? WHERE id=?";
+    private static final String FIND_ADMIN_QUERY = "SELECT * FROM admins WHERE id=?";
     public static String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
@@ -27,7 +27,7 @@ public class AdminDao  {
     public Admin read(Integer adminId) {
         Admin admin = new Admin();
         try (Connection connection = DbUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(findAdminQuery)
+             PreparedStatement statement = connection.prepareStatement(FIND_ADMIN_QUERY)
         ) {
             statement.setInt(1, adminId);
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -51,7 +51,7 @@ public class AdminDao  {
     public List<Admin> findAll() {
         List<Admin> adminList = new ArrayList<>();
         try (Connection connection = DbUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(findAllAdminsQuery);
+             PreparedStatement statement = connection.prepareStatement(FIND_ALL_ADMIN_QUERY);
              ResultSet resultSet = statement.executeQuery()) {
             while (resultSet.next()) {
                 Admin admin = new Admin();
@@ -74,7 +74,7 @@ public class AdminDao  {
 
     public Admin create(Admin admin) {
         try (Connection connection = DbUtil.getConnection();
-             PreparedStatement insertStm = connection.prepareStatement(createAdminQuery,
+             PreparedStatement insertStm = connection.prepareStatement(CREATE_ADMIN_QUERY,
                      PreparedStatement.RETURN_GENERATED_KEYS)) {
             insertStm.setString(1, admin.getFirstName());
             insertStm.setString(2, admin.getLastName());
@@ -103,7 +103,7 @@ public class AdminDao  {
 
     public void delete(Integer adminId) {
         try (Connection connection = DbUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(deleteAdminQuery)) {
+             PreparedStatement statement = connection.prepareStatement(DELETE_ADMIN_QUERY)) {
             statement.setInt(1, adminId);
             statement.executeUpdate();
 
@@ -118,7 +118,7 @@ public class AdminDao  {
 
     public void update(Admin admin) {
         try (Connection connection = DbUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(updateAdminQuery)) {
+             PreparedStatement statement = connection.prepareStatement(UPDATE_ADMIN_QUERY)) {
             statement.setInt(5, admin.getId());
             statement.setString(1, admin.getFirstName());
             statement.setString(2, admin.getLastName());
@@ -133,7 +133,7 @@ public class AdminDao  {
 
     public void changeIfSuperadmin(Integer adminId, Integer ifSuperadmin){
         try (Connection connection = DbUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(changeSuperadminQuery)) {
+             PreparedStatement statement = connection.prepareStatement(CHANGE_SUPER_ADMIN_QUERY)) {
             statement.setInt(2, adminId);
             statement.setInt(1, ifSuperadmin);
             statement.executeUpdate();
