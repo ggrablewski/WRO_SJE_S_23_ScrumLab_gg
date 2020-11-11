0790a20f4df05ee475bf599be87fc2e7ee2f4b40
Jabberski <chalasiewicz97@gmail.com>
2020-11-11 01:12:16 +0100

    liczenie planów- sprawdzone

--- src/main/java/pl/coderslab/web/IndexServlet.java
+++ src/main/java/pl/coderslab/web/IndexServlet.java
@@ -15,7 +15,6 @@ import java.util.List;
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
         getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
