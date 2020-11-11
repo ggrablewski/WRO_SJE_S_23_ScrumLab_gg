5fcdc51e855038b9d3d6581c2912cdadfde41081
Jabberski <chalasiewicz97@gmail.com>
2020-11-11 12:59:33 +0100

    podstawowy dashboard i poprawiony filter

--- src/main/java/pl/coderslab/web/LoggedFilter.java
+++ src/main/java/pl/coderslab/web/LoggedFilter.java
@@ -1,21 +1,23 @@
 package pl.coderslab.web;
 
-import javax.servlet.Filter;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebFilter("/app/*")
 public class LoggedFilter implements Filter {
 
-    public void doFilter(javax.servlet.ServletRequest req, javax.servlet.ServletResponse resp, javax.servlet.FilterChain chain) throws javax.servlet.ServletException, java.io.IOException {
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpSession session = ((HttpServletRequest) req).getSession(false);
         Boolean isLogged = session != null && (Boolean) session.getAttribute("ifLogged");
         if (isLogged) {
             chain.doFilter(req, resp);
+
         } else {
-            ((HttpServletResponse) resp).sendRedirect("/login.jsp");
+            ((HttpServletResponse) resp).sendRedirect("/login");
         }
     }
 }
