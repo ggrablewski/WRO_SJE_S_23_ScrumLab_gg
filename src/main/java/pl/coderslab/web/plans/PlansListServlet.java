0595af3e8c2efaa3aa3b5cf3cb58593eeb069711
zeluzel <zeluzel@gmail.com>
2020-11-12 17:15:27 +0100

    gotowe

--- src/main/java/pl/coderslab/web/plans/PlansListServlet.java
+++ src/main/java/pl/coderslab/web/plans/PlansListServlet.java
@@ -1,8 +1,6 @@
 package pl.coderslab.web.plans;
-
 import pl.coderslab.dao.PlanDao;
 import pl.coderslab.model.Plan;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/app/plan/list")
+@WebServlet("/plan-list")
 public class PlansListServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int adminId = (int) request.getSession().getAttribute("adminId");
         List<Plan> adminPlans = PlanDao.findAllForAdmin(adminId);
         request.setAttribute("adminPlans", adminPlans);
-        getServletContext().getRequestDispatcher("/app/plans-list.jsp");
+        getServletContext().getRequestDispatcher("/app/plan-list.jsp").forward(request,response);
     }
 }
